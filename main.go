18fe6f4e8535d15ca98a0da870121dd4935bbb03
prashanth <prashanth@celominds.com>
2020-03-14 02:09:13 +0530

    Added POST request

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"io"
+	"io/ioutil"
 	"github.com/julienschmidt/httprouter"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome to Meant4")
+	fmt.Fprint(w, "Welcome!\n")
+}
+type CalculateParams struct {
+	aParam   int `json:"a"`
+	bParam  int `json:"b"`
 }
 
 type JsonResponse struct {
@@ -16,9 +23,33 @@ type JsonResponse struct {
 	Status interface{} `json:"status"`
 }
 
+func CalculateFactorial(rw http.ResponseWriter, r *http.Request, hp httprouter.Params) {
+	var calculateParams CalculateParams
+
+    fmt.Fprintln(rw, "Calculating Factorial")
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+    if err != nil {
+        log.Fatal(err)
+    }
+    if err := r.Body.Close(); err != nil {
+        log.Fatal(err)
+    }
+    if err := json.Unmarshal(body,calculateParams); err != nil {
+		response := &JsonResponse{Data: &calculateParams}
+		rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		rw.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(rw).Encode(response); err != nil {
+			panic(err)
+		}
+    }
+ 
+}
+
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
+	router.POST("/calculate", CalculateFactorial)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
